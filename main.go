2c2c6a0dd3edffdff0c8e2776ef167eaf5212ad3
Patrick Flynn <patrick@chainguard.dev>
2022-05-18 13:03:27 -0400

    touch the server to generate new container sha
    Signed-off-by: Patrick Flynn <patrick@chainguard.dev>

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 func main() {
     http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hello, World!")
+        fmt.Fprintf(w, "Hello, Unsigned World!")
     })
     fmt.Printf("Server running (port=8080), route: /helloworld\n")
     if err := http.ListenAndServe(":8080", nil); err != nil {
